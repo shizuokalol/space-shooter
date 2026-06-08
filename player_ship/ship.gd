6ca2687f323f0824d20393371d8e321639733ad1
shizuokalol <123810070+shizuokalol@users.noreply.github.com>
2026-06-08 17:56:20 +0300

    Animate ship

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,13 +5,32 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
 @onready var fire_rate_rimer: Timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent
+@onready var animated_sprite: AnimatedSprite2D = $Anchor/AnimatedSprite2D
+@onready var flame_animated_sprite: AnimatedSprite2D = $Anchor/FlameAnimatedSprite
+@onready var move_component: MoveComponent = $MoveComponent
 
 
 func _ready() -> void:
 	fire_rate_rimer.timeout.connect(fire_lasers)
 
 
+func _process(_delta: float) -> void:
+	animate_ship()
+
+
 func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+
+
+func animate_ship() -> void:
+	if move_component.velocity.x < 0.0:
+		animated_sprite.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif move_component.velocity.x > 0.0:
+		animated_sprite.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite.play("center")
+		flame_animated_sprite.play("center")
