c643ebc8dbd64c7064faee99c87f1fd5ecd3d765
shizuokalol <123810070+shizuokalol@users.noreply.github.com>
2026-06-22 19:45:53 +0300

    Create game over score and highscore

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,6 +1,20 @@
 extends Control
 
+@export var game_stats: GameStats
+
+@onready var score_value_label: Label = %ScoreValueLabel
+@onready var highscore_value_label: Label = %HighscoreValueLabel
+
+
+func _ready() -> void:
+	if game_stats.score > game_stats.high_score:
+		game_stats.high_score = game_stats.score
+
+	score_value_label.text = str(game_stats.score)
+	highscore_value_label.text = str(game_stats.high_score)
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
+		game_stats.score = 0
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
